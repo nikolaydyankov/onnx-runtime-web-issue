12754cb5c61af513e52c820147a995922af8c0e8
nikolaydyankov <dyankov.nikolay@gmail.com>
2023-09-26 14:14:09 +0300

    initial

---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { InferenceSession } from 'onnxruntime-web'
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    const session = InferenceSession.create('sam_onnx_quantized.onnx')
+  })
+</script>
+
+<a href="/">/home</a>
